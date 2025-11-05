8b5199bb46e6b70bef25495544e664c175450fce
Stahelstrauch <estahelstrauchpulk@gmail.com>
2025-11-05 10:33:58 +0200

    First Commit

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+// import './bootstrap';
+import * as bootstrap from 'bootstrap'
+
+// Tee bootstrap globaalseks
+window.bootstrap = bootstrap
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title', 'Blog | Avaleht')
+@section('content')
+
+Test
+
+@endsection
